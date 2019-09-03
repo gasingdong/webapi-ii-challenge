0d75b9463dc8995475868cfe32e6b86566b699bb
Brandon Dong <gasingdong@gmail.com>
2019-09-03 12:30:06 -0700

    get all posts endpoint

--- api/posts/postsRoutes.js
+++ api/posts/postsRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const db = require('../../data/db');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send('hello from GET /posts endpoint');
+  (async () => {
+    try {
+      const posts = await db.find();
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    }
+  })();
 });
 
 module.exports = router;
