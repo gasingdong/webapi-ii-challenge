ab189bebbb97452207874cf8da99742e9068f77f
Brandon Dong <gasingdong@gmail.com>
2019-09-03 12:38:40 -0700

    get post by id endpoint

--- api/posts/postsRoutes.js
+++ api/posts/postsRoutes.js
@@ -14,4 +14,19 @@ router.get('/', (req, res) => {
   })();
 });
 
+router.get('/:id', (req, res) => {
+  (async () => {
+    try {
+      const post = await db.findById(req.params.id);
+      if (post.length === 0) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+        return;
+      }
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json({ error: "The post information could not be retrieved." });
+    }
+  })();
+});
+
 module.exports = router;
