9f3ad791064d66f981d1efe03d9b78203688737d
Brandon Dong <gasingdong@gmail.com>
2019-09-03 13:29:53 -0700

    update post endpoint

--- api/posts/postsRoutes.js
+++ api/posts/postsRoutes.js
@@ -114,4 +114,31 @@ router.delete('/:id', (req, res) => {
   })();
 });
 
+router.put('/:id', (req, res) => {
+  (async () => {
+    try {
+      const post = await db.findById(req.params.id);
+      if (post.length === 0) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+        return;
+      }
+      const { title, contents } = req.body;
+      if (title && contents) {
+        const timestamp = Date.now();
+        const newPost = {
+          title,
+          contents,
+          updated_at: timestamp,
+        }
+        await db.update(post[0].id, newPost);
+        res.status(200).json(newPost);
+      } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+      }
+    } catch (err) {
+      res.status(500).json({ error: "The post information could not be modified." });
+    }
+  })();
+})
+
 module.exports = router;
