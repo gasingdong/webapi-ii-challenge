b256b96ebf4baeabb999ef0d7342f570ca6c2f3f
Brandon Dong <gasingdong@gmail.com>
2019-09-03 12:21:15 -0700

    add server routing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./data/db');
+const apiRoutes = require('./api/apiRoutes');
 
 const server = express();
 
+server.use('/api', apiRoutes);
 
 const port = 5000;
 server.listen(port, () => console.log(`Server running on http://localhost:${port}/`));
